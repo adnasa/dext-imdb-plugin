4b536b244b0d6c67a19ab922b147ae57f887c058
Adnan Asani <konjuku@gmail.com>
2016-09-11 22:25:17 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# dext-omdb-plugin
+
+> [Dext](https://github.com/vutran/dext) plugin to search movies from [omdb](http://www.omdbapi.com/).
+
+![img](omdb-got.png)
+
+Although you are redirected to imdb, this plugin uses the omdbapi to search for movies.
+imdb has not released an open-api as far as I am aware.
+
+## Installation
+
+```
+$ dpm install dext-omdb-plugin
+```
---
+++ index.js
@@ -0,0 +1,57 @@
+const request = require('request')
+const qs = require('querystring')
+
+const BASE_URL = 'http://www.omdbapi.com/'
+
+const OPTS_DEFAULT = {
+  r: 'json',
+  plot: 'short',
+  t: null,
+}
+
+module.exports = {
+  keyword: 'omdb',
+  helper: {
+    title: 'omdb',
+    subtitle: 'Find a movie you\'re curious about',
+  },
+  execute: q => new Promise(resolve => {
+    const searchObj = Object.assign({}, OPTS_DEFAULT, { t: q })
+    const searchParams = qs.stringify(searchObj)
+    const url = `${BASE_URL}?${searchParams}`
+    request.get({ url }, (err, responseObj, body) => {
+      if (err) { resolve({ items: [] }); return }
+
+      const response = JSON.parse(body)
+      if (!isTruthyResponse(response)) {
+        resolve({ items: [] })
+        return
+      }
+
+      const resultItem = {
+        title: response.Title,
+        // @TODO: Truncate the Plot when it is long
+        subtitle: `${response.imdbRating} - ${response.Plot}`,
+        arg: `http://imdb.com/title/${response.omdbID}`,
+      }
+
+      resolve({ items: [resultItem] })
+    })
+  }),
+  render: {
+    type: 'html',
+    render,
+  },
+}
+
+function render (item) {
+  // @TODO: Add more info into the panel
+  return `
+    <h3>${item.Title}</h3>
+    <p>${item.Plot}</p>
+  `
+}
+
+function isTruthyResponse ({ Response }) {
+  return Response === 'True'
+}
